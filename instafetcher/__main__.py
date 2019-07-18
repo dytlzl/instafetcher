7dda07ae3f1dae66aa5ad14bd8d9938ebd41dc4a
new <new@example.com>
2019-07-18 13:44:14 +0900

    Subdivide catching errors

--- instafetcher/__main__.py
+++ instafetcher/__main__.py
@@ -16,19 +16,28 @@ def main():
 
 
 def fetch(url):
-    try:
-        if "instagram.com/p/" in url:
+    if "instagram.com/p/" in url:
+        try:
             shortcode = re.search(r'instagram.com/p/(.*?)/', url).groups()[0]
-            instafetcher.Post(shortcode).fetch()
-        elif "instagram.com/explore/tags/" in url:
-            tag = re.search(r'instagram.com/explore/tags/(.*?)/', url).groups()[0]
-            instafetcher.Tag(tag).fetch()
-        elif "instagram.com/" in url:
-            username = re.search(r'instagram.com/(.*?)/', url).groups()[0]
-            instafetcher.Profile(username).fetch()
-        else:
+        except AttributeError:
             print('Invalid URL')
-    except (IndexError, AttributeError):
+        else:
+            instafetcher.Post(shortcode).fetch()
+    elif "instagram.com/explore/tags/" in url:
+        try:
+            tag = re.search(r'instagram.com/explore/tags/(.*?)/', url).groups()[0]
+        except AttributeError:
+            print('Invalid URL')
+        else:
+            instafetcher.Tag(tag).fetch()
+    elif "instagram.com/" in url:
+        try:
+            username = re.search(r'instagram.com/(.*?)/', url).groups()[0]
+        except AttributeError:
+            print('Invalid URL')
+        else:
+            instafetcher.Profile(username).fetch()
+    else:
         print('Invalid URL')
 
 
